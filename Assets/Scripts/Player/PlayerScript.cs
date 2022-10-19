c88b6497707a9b76598ba81be3a88d3f17d37507
William Fox <william.g.c.fox@gmail.com>
2022-10-19 22:23:21 +0100

    Exposed timer for stun
    Exposed timer for stun

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -13,6 +13,7 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private SpriteRenderer plWepSprite;
     [SerializeField] private Animator plAnim;
     [SerializeField] private Animator plWepAnim;
+    [SerializeField] private float stun = 3.0f;
 
     private const float shieldSpeed = 3.0f;
     private Rigidbody2D plRigid;
@@ -259,7 +260,7 @@ public class PlayerScript : MonoBehaviour
     private IEnumerator GhostBind()
     {
         playerEnabled = false;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(stun);
         playerEnabled = true;
     }
 
